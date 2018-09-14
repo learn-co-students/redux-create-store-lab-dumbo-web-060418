8df5640cd70d3fd1ca783c9934b604a954f30505
QMaximillian <quinnlashinsky@gmail.com>
2018-09-14 10:14:28 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,24 @@
+// import candyReducer from './reducers/candyReducer.js'
+
+
+
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state
+  }
+
+  dispatch({type: '@@INIT'})
+
+  return {
+    dispatch, getState
+  }
 }
 
 function render() {
